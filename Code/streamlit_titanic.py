7be0aea1b7aa51a2da96e6f0c0b1f77d55fda471
Arnau Nin <ninbsnss92@gmail.com>
2024-10-16 17:53:04 +0200

    Some code

--- Code/streamlit_titanic.py
+++ Code/streamlit_titanic.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title = "Titanic - data",
+    page_icon = "🚢",
+    layout = "wide",
+    initial_sidebar_state = "collapsed"
+)
+
+st.header("Titanic passenger data")
+
+st.write("Hola mundo")
