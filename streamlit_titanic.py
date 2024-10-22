6deb65c5e5db9546b88609d51984006f0073c7fc
Arnau Nin <ninbsnss92@gmail.com>
2024-10-22 11:51:39 +0200

    AI code

--- streamlit_titanic.py
+++ streamlit_titanic.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import copy
+import plotly.express as px
+from titanic_ai_model import get_model
 
 def clean_data(df):
 
@@ -46,6 +49,56 @@ def update_df():
         ((st.session_state["df"]["Fare"] >= st.session_state["Fare"][0]) & (st.session_state["df"]["Fare"] <= st.session_state["Fare"][1]))
      ]
 
+def generate_plot(var1, var2, var3, color_var,num_var, plot_type):
+
+    if plot_type == "Bar":
+
+        fig = px.bar(
+            st.session_state['df_fil'],
+            x = var1,
+            y = num_var,
+            color = color_var
+        )
+
+    elif plot_type == "Pie":
+
+        fig = px.pie(
+            st.session_state['df_fil'],
+            values = num_var,
+            names = var1
+        )
+
+    elif plot_type == "Scatter":
+
+        fig = px.scatter(
+            st.session_state['df_fil'],
+            x = var1,
+            y = var2,
+            size = num_var,
+            color = color_var
+        )
+
+    elif plot_type == "Heatmap":
+
+        fig = px.density_heatmap(
+            st.session_state['df_fil'],
+            x = var1,
+            y = var2,
+            z = num_var,
+            text_auto = True
+        )
+
+    elif plot_type == "Treemap":
+        
+        fig = px.treemap(
+            st.session_state['df_fil'],
+            path = [var1, var2, var3],
+            values = num_var,
+            color = color_var
+        )
+
+    return fig
+
 st.set_page_config(
     page_title = "Titanic - data",
     page_icon = "🚢",
@@ -53,10 +106,10 @@ st.set_page_config(
     initial_sidebar_state = "collapsed"
 )
 
-if 'df' in st.session_state:
+if 'df' not in st.session_state:
     st.session_state['df'] = pd.DataFrame()
 
-if 'df_fil' in st.session_state:
+if 'df_fil' not in st.session_state:
     st.session_state['df_fil'] = pd.DataFrame()
 
 st.header("Titanic passenger data")
@@ -79,7 +132,50 @@ def page_1():
         st.write(st.session_state['df'].describe())
 
 def page_2():
+
+    if st.session_state['df_fil'].empty:
+        st.session_state['df_fil'] = copy.copy(st.session_state['df'])
+        
+
     st.subheader("Data analitiscs📈")
+
+    col_1_plot, col_2_plot = st.columns([5,1])
+
+    with col_1_plot:
+        fig_plot = st.empty()
+
+    with col_2_plot:
+
+        plot_type = st.selectbox(
+            "Type of plot",
+            options = ["Bar", "Pie", "Scatter", "Heatmap", "Treemap"]
+        )
+
+        var1 = st.selectbox(
+            "1st variable",
+            options = st.session_state["df_fil"].columns,
+        )
+
+        num_var = st.selectbox(
+            "Numeric variable",
+            options = ['count', 'Fare', 'Age', 'SibSp', 'Parch']
+        )
+
+        color_var = st.selectbox(
+            "Color variable",
+            options = st.session_state["df_fil"].columns,
+        )
+
+        var2 = st.selectbox(
+            "2nd variable",
+            options = st.session_state["df_fil"].columns
+        )
+
+        var3 = st.selectbox(
+            "3rd variable",
+            options = st.session_state["df_fil"].columns
+        )
+    
 
     with st.expander("Filters"):
             
@@ -168,11 +264,90 @@ def page_2():
         if submit:
             update_df()
 
-        st.write(st.session_state['df_fil'])
-
+    fig = generate_plot(var1, var2, var3, color_var,num_var, plot_type)
+    fig_plot.write(fig)
 
 def page_3():
     st.subheader("Artificial inteligence🤖")
+
+    model = get_model()
+
+    with st.form("Prediction form"):
+
+        col_pred_1, col_pred_2, col_pred_3 = st.columns([1,1,1])
+    
+        with col_pred_1:
+            class_input = st.selectbox(
+                    "Class",
+                    options = [1,2,3]
+                    )
+                    
+            age_input = st.number_input(
+                    "Age",
+                    min_value = 0.0,
+                    max_value = 100.0,
+                    )
+                    
+            sibsp_input = st.number_input(
+                    "Siblings",
+                    min_value = 0,
+                    max_value = 10,
+                    )
+                    
+        with col_pred_2:
+            parch_input = st.number_input(
+                    "Parents/Children",
+                    min_value = 0,
+                    max_value = 10,
+                    )
+                    
+            fare_input = st.number_input(
+                    "Fare",
+                    min_value = 0.0,
+                    max_value = 1000.0,
+                    )
+        
+        with col_pred_3:        
+            sex_input = st.toggle(
+                    "Sex male",
+                    )
+                    
+            q_input = st.toggle(
+                    "Queenstown embarked",
+                    )
+                    
+            s_input = st.toggle(
+                    "Southhampton embarked",
+                    )
+
+            submit_prediction = st.form_submit_button("Submit prediction")
+
+        if submit_prediction:
+            
+            input_vector = [[
+                    class_input,
+                    age_input,
+                    sibsp_input,
+                    parch_input,
+                    fare_input,
+                    sex_input,
+                    q_input,
+                    s_input,
+                    ]]
+            
+            y_pred = model.predict(input_vector)
+
+            if y_pred:
+                
+                st.success("The passenger is likely to survive!")
+                
+            else:
+                
+                st.error("The passenger is likely to die...")
+
+st.title("Titanic passenger data")
+
+
 
 pg = st.navigation(
     {"D&A": [
